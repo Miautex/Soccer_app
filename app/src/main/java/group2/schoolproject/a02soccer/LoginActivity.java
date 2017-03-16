8bd59276615a31ece4d1a8dcd77ce8bdd02a3da0
Martin <martin.sonnberger@live.at>
2017-03-16 15:41:38 +0100

    some additions

---
+++ app/src/main/java/group2/schoolproject/a02soccer/LoginActivity.java
@@ -0,0 +1,13 @@
+package group2.schoolproject.a02soccer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
