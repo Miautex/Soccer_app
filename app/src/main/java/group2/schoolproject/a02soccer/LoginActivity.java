c18368ee7ee9f87d024dbc4e955ae19574f7c386
Martin <martin.sonnberger@live.at>
2017-03-20 19:09:05 +0100

    added functionality to login activity

--- app/src/main/java/group2/schoolproject/a02soccer/LoginActivity.java
+++ app/src/main/java/group2/schoolproject/a02soccer/LoginActivity.java
@@ -1,13 +1,83 @@
 package group2.schoolproject.a02soccer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnLogin = null;
+    EditText edtPassword = null;
+    EditText edtUsername = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getAllViews();
+        registrateEventHandlers();
     }
 
+    /**
+     * set all views by id
+     */
+    private void getAllViews(){
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        edtUsername = (EditText) findViewById(R.id.edtUsername);
+    }
+
+    /**
+     * registrate all EventHandlers
+     */
+    private void registrateEventHandlers(){
+        btnLogin.setOnClickListener(this);
+    }
+
+    public void onClick(View arg0) {
+        try {
+            if (arg0.getId() == R.id.btnLogin) {
+                tryLogin();
+            }
+        } catch (Exception e) {
+            showToast("Error with onClick");
+        }
+    }
+
+    /**
+     * try to login and if successful open the MainActivity
+     */
+    private void tryLogin(){
+        //Datenbank Abfrage
+        boolean success = true;
+        if(success){
+            showToast("Login successful");
+            openMainActivity();
+        }
+    }
+
+    /**
+     * opens MainActivity
+     */
+    private void openMainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * shows Toast == MessageLine
+     * @param text Message you want to display
+     */
+    private void showToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
