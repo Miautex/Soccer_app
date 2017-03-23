85bea2c98b09c0ba35015143709a5a59d882b06b
Elias <elias.santner@edu.htl-villach.at>
2017-03-23 15:43:35 +0100

    Updated Database class

--- app/src/main/java/pkgData/Database.java
+++ app/src/main/java/pkgData/Database.java
@@ -1,20 +1,19 @@
 package pkgData;
 
+import android.app.Application;
+
 import java.util.ArrayList;
-import java.util.TreeMap;
 
 /**
  * Created by Elias on 22.03.2017.
  */
 
-public class Database {
+public class Database extends Application {
     private static Database instance = null;
-    private TreeMap<Integer, Player> mapPlayers = null;
-
+    private ArrayList<DatabaseObject> listDbos = null;
 
     private Database() throws Exception {
-        mapPlayers = new TreeMap<>();
-
+        listDbos = new ArrayList<>();
         generateTestData();
     }
 
@@ -26,38 +25,59 @@ public class Database {
     }
 
     public ArrayList<Player> getPlayers() throws Exception {
-        return new ArrayList<>(mapPlayers.values());
-    }
+        ArrayList<Player> listPlayers = new ArrayList<>();
 
-    public void addPlayer(Player p) throws Exception {
-        if (mapPlayers.containsKey(p.getId())) {
-            throw new Exception("Player with id=" + p.getId() + " already exists");
+        for (DatabaseObject dbo: listDbos) {
+            if (dbo.getClass().equals(Player.class)) {
+                listPlayers.add((Player) dbo);
+            }
         }
 
-         mapPlayers.put(p.getId(), p);
+        return listPlayers;
     }
 
-    public void updatePlayer(Player p) throws Exception {
-        if (!mapPlayers.containsKey(p.getId())) {
-            throw new Exception("Player with id=" + p.getId() + " doesn't exist, so it cannot be updated");
+    public void insert(DatabaseObject dbo) throws Exception {
+        if (listDbos.contains(dbo)) {
+            throw new Exception(dbo.getClass() + " with id=" + dbo.getId() + " already exists");
         }
-
-        mapPlayers.remove(p.getId());
-        mapPlayers.put(p.getId(), p);
+        listDbos.add(dbo);
     }
 
-    public void removePlayer(int id) throws Exception {
-         mapPlayers.remove(id);
+    public void update(DatabaseObject dbo) throws Exception {
+        if (!listDbos.contains(dbo)) {
+            throw new Exception(dbo.getClass() + " with id=" + dbo.getId() + " doesn't exist, so it cannot be updated");
+        }
+        listDbos.remove(dbo);
+        listDbos.add(dbo);
+    }
+
+    public void remove(DatabaseObject dbo) throws Exception {
+        if (!listDbos.contains(dbo)) {
+            throw new Exception(dbo.getClass() + " with id=" + dbo.getId() + " doesn't exist, so it cannot be deleted");
+        }
+        listDbos.remove(dbo);
+    }
+
+    public void commit() {
+        //TODO
+    }
+
+    public void rollback() {
+        //TODO
     }
 
     private void generateTestData() throws Exception {
-         mapPlayers.put(1, new Player(1, "martin", "Martin"));
-         mapPlayers.put(2, new Player(2, "elias", "Elias"));
-         mapPlayers.put(3, new Player(3, "marco", "Marco"));
-         mapPlayers.put(4, new Player(4, "raphael", "Raphael"));
-         mapPlayers.put(5, new Player(5, "pascal", "Pascal"));
-         mapPlayers.put(6, new Player(6, "jakob", "Jakob"));
-         mapPlayers.put(7, new Player(7, "stefan", "Stefan"));
-         mapPlayers.put(8, new Player(8, "lukas", "Lukas"));
+        generateTestPlayers();
+    }
+
+    private void generateTestPlayers() throws Exception {
+        listDbos.add(new Player(1, "martin", "Martin"));
+        listDbos.add(new Player(2, "elias", "Elias"));
+        listDbos.add(new Player(3, "marco", "Marco"));
+        listDbos.add(new Player(4, "raphael", "Raphael"));
+        listDbos.add(new Player(5, "pascal", "Pascal"));
+        listDbos.add(new Player(6, "jakob", "Jakob"));
+        listDbos.add(new Player(1, "martin", "Martin"));
+        listDbos.add(new Player(1, "martin", "Martin"));
     }
 }
