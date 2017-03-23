5e13753cfb2e4bc22bc6bc1b6f5269e0d5507243
Elias <elias.santner@edu.htl-villach.at>
2017-03-23 15:14:58 +0100

    Changed DatabaseObject to Interface

--- app/src/main/java/pkgData/DatabaseObject.java
+++ app/src/main/java/pkgData/DatabaseObject.java
@@ -1,13 +1,9 @@
 package pkgData;
 
-import java.io.Serializable;
-
 /**
  * Created by Elias on 23.03.2017.
  */
 
-public abstract class DatabaseObject implements Comparable, Serializable {
-    private int id;
-
+public interface DatabaseObject {
     public abstract int getId();
 }
