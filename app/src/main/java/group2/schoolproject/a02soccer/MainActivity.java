fba2e0da32e7d29719d60c7f83c43418ae8b6356
Martin <martin.sonnberger@live.at>
2017-03-23 15:07:14 +0100

    added some very impotent things

--- app/src/main/java/group2/schoolproject/a02soccer/MainActivity.java
+++ app/src/main/java/group2/schoolproject/a02soccer/MainActivity.java
@@ -1,7 +1,53 @@
 package group2.schoolproject.a02soccer;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    ListView lsvEditableGames = null;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getAllViews();
+        showToast("created");
+
+    }
+
+    private void getAllViews(){
+        lsvEditableGames = (ListView) findViewById(R.id.lsvEditableGames);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mniAddUser:
+                showToast("Add User clicked");
+                break;
+            case R.id.mniEditUser:
+                showToast("Edit User clicked");
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    private void showToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
