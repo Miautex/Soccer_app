bbce4c044efdd881bb7d532e37276118a4852736
wilschermarco <wilschermarco@users.noreply.github.com>
2017-04-30 10:36:27 +0200

    Added param body

--- app/src/main/java/pkgWSA/Accessor.java
+++ app/src/main/java/pkgWSA/Accessor.java
@@ -12,7 +12,7 @@ import java.net.URL;
 public final class Accessor {
     private static String serverUrl = "http://192.168.194.27:8080/team02/services/";
 
-    public static AccessorResponse requestJSON(HttpMethod method, String servicePath, String serviceQuery) throws Exception {
+    public static AccessorResponse requestJSON(HttpMethod method, String servicePath, String serviceQuery, String body) throws Exception {
         URL url = null;
         HttpURLConnection con = null;
         int responseCode = 0;
@@ -39,6 +39,11 @@ public final class Accessor {
             con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             con.setRequestProperty("Accept", "application/json; charset=UTF-8");
 
+            if (body != null) {
+                con.setRequestProperty("Content-Length", Integer.toString(body.length()));
+                con.getOutputStream().write(body.getBytes("UTF8"));
+            }
+            
             responseCode = con.getResponseCode();
             reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
             jsonStringBuffer = new StringBuffer();
