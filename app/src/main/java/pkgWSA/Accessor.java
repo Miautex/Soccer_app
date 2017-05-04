6da2e9afa1bf1d407f11772131254d07f1e4a139
wilschermarco <wilschem@edu.htl-villach.at>
2017-05-04 15:31:35 +0200

    DNS support for WebService

--- app/src/main/java/pkgWSA/Accessor.java
+++ app/src/main/java/pkgWSA/Accessor.java
@@ -5,13 +5,15 @@ package pkgWSA;
  */
 
 public final class Accessor {
-    private final static String serverUrl = "http://192.168.194.27:8080/team02/services/";
+    private static String serverUrl = "http://{ip}:8080/team02/services/";//"http://192.168.194.27:8080/team02/services/";
 
     public static AccessorResponse requestJSON(HttpMethod method, String servicePath, String serviceQuery, String body) throws Exception {
+        String url = serverUrl.replace("{ip}", java.net.InetAddress.getByName("MWPC").getHostAddress());
+
         AccessorResponse response;
         WebServiceTask task = new WebServiceTask();
 
-        task.execute(new TaskParams(serverUrl, method, servicePath, serviceQuery, body));
+        task.execute(new TaskParams(url, method, servicePath, serviceQuery, body));
         response = task.get();
         if (response.getException() != null) {
             throw response.getException();
