7dd8a1fe874eaeaba284ae0f1abd78bbed5c1054
wilschermarco <wilschermarco@users.noreply.github.com>
2017-05-05 07:42:38 +0200

    webservice ip

--- app/src/main/java/pkgWSA/Accessor.java
+++ app/src/main/java/pkgWSA/Accessor.java
@@ -5,15 +5,13 @@ package pkgWSA;
  */
 
 public final class Accessor {
-    private static String serverUrl = "http://{ip}:8080/team02/services/";//"http://192.168.194.27:8080/team02/services/";
+    private static String serverUrl = "http://192.168.196.2:25120/team02/services/";//"http://192.168.194.27:8080/team02/services/";
 
-    public static AccessorResponse requestJSON(HttpMethod method, String servicePath, String serviceQuery, String body) throws Exception {
-        String url = serverUrl.replace("{ip}", java.net.InetAddress.getByName("MWPC").getHostAddress());
-
+    public static AccessorResponse requestJSON(HttpMethod method, String servicePath, String serviceQuery, String body) throws Exception {       
         AccessorResponse response;
         WebServiceTask task = new WebServiceTask();
 
-        task.execute(new TaskParams(url, method, servicePath, serviceQuery, body));
+        task.execute(new TaskParams(serverUrl, method, servicePath, serviceQuery, body));
         response = task.get();
         if (response.getException() != null) {
             throw response.getException();
