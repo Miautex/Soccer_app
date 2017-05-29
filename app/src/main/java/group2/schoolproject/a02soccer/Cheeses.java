efcab47d888190d6ec6475228b522a80bd22b5e4
Raphy998 <raphaelmoser@gmx.at>
2017-05-29 13:42:12 +0200

    chesus 2x

--- app/src/main/java/group2/schoolproject/a02soccer/Cheeses.java
+++ app/src/main/java/group2/schoolproject/a02soccer/Cheeses.java
@@ -5,7 +5,7 @@ package group2.schoolproject.a02soccer;
 public class Cheeses {
 
     public static final String[] sCheeseStrings = {
-            "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Santners Füße","Fresh Cheese" ,"Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
             "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale",
             "Aisy Cendre", "Allgauer Emmentaler", "Alverca", "Ambert", "American Cheese",
             "Ami du Chambertin", "Anejo Enchilado", "Anneau du Vic-Bilh", "Anthoriro", "Appenzell",
