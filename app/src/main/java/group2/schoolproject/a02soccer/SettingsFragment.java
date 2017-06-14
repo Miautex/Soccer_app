0b00c7a61d7b905887707f94ee40f7abb34ee951
wilschermarco <wilschem@edu.htl-villach.at>
2017-06-14 08:47:15 +0200

    layoutpicker check if admin

--- app/src/main/java/group2/schoolproject/a02soccer/SettingsFragment.java
+++ app/src/main/java/group2/schoolproject/a02soccer/SettingsFragment.java
@@ -1,8 +1,13 @@
 package group2.schoolproject.a02soccer;
 
 import android.os.Bundle;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+
+import pkgData.Player;
+import pkgDatabase.Database;
+import pkgPreference.LayoutPickerPreference;
 
 /**
  * @author Marco Wilscher
@@ -12,5 +17,11 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate (@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        Player p = Database.getInstance().getCurrentlyLoggedInPlayer();
+        if (p != null && !p.isAdmin()) {
+            PreferenceCategory cat = (PreferenceCategory) findPreference("preference_general");
+            LayoutPickerPreference lpp = (LayoutPickerPreference)findPreference("preference_assignment_layout");
+            cat.removePreference(lpp);
+        }
     }
 }
